24a78621c87054859cfd8b6f17e0d9c4054dedbf
fengchaohnsq <41362868+fengchaohnsq@users.noreply.github.com>
2019-04-19 06:11:06 -0500

    Add files via upload

---
+++ face merging/merge and swap/faceMergeSwap.py
@@ -0,0 +1,309 @@
+#! /usr/bin/env python
+
+
+# 对静态人脸图像文件进行68个特征点的标定
+import dlib         # 人脸识别的库 Dlib
+import numpy as np,numpy  # 数据处理的库 numpy
+import cv2   # 图像处理的库 OpenCv
+import sys
+
+
+# Dlib 检测器和预测器
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+
+#获取68个面孔特征点，输入函数是图片，返回68个图片的像素点数组，数组index从0开始
+def get_landmarks(img):
+    faces = detector(img, 1)# 人脸数，第二个参数inclass为1时检测RGB图片，参数为0时检测灰度图片
+    if len(faces) == 1:
+        return np.matrix([[p.x, p.y] for p in predictor(img, faces[0]).parts()])
+    raise NoFaceOrTooManyFaces
+
+
+
+# 从text文本中读取特征点
+def readPoints(path) :
+    # Create an array of points.
+    points = [];
+    # Read points
+    with open(path) as file :
+        for line in file :
+            x, y = line.split()
+            points.append((int(x), int(y)))
+
+    return points
+
+# Apply affine transform calculated using srcTri and dstTri to src and
+# output an image of size.
+def applyAffineTransform(src, srcTri, dstTri, size) :
+  
+    # Given a pair of triangles, find the affine transform.
+    warpMat = cv2.getAffineTransform( np.float32(srcTri), np.float32(dstTri) )
+    
+    # Apply the Affine Transform just found to the src image
+    #像素使用线性插值的方式来对RGB颜色通道进行融合，flags=cv2.INTER_LINEAR
+    dst = cv2.warpAffine( src, warpMat, (size[0], size[1]), None, flags=cv2.INTER_LINEAR, borderMode=cv2.BORDER_REFLECT_101 )
+
+    return dst
+
+# Warps and alpha blends triangular regions from img1 and img2 to img
+def morphTriangle(img1, img2, img, t1, t2, t, alpha) :
+
+    # Find bounding rectangle for each triangle
+    r1 = cv2.boundingRect(np.float32([t1]))
+    r2 = cv2.boundingRect(np.float32([t2]))
+    r = cv2.boundingRect(np.float32([t]))
+
+    # Offset points by left top corner of the respective rectangles
+    t1Rect = []
+    t2Rect = []
+    tRect = []
+
+    for i in range(0, 3):
+        tRect.append(((t[i][0] - r[0]),(t[i][1] - r[1])))
+        t1Rect.append(((t1[i][0] - r1[0]),(t1[i][1] - r1[1])))
+        t2Rect.append(((t2[i][0] - r2[0]),(t2[i][1] - r2[1])))
+
+
+    # Get mask by filling triangle
+    mask = np.zeros((r[3], r[2], 3), dtype = np.float32)
+    cv2.fillConvexPoly(mask, np.int32(tRect), (1.0, 1.0, 1.0), 16, 0);
+
+    # Apply warpImage to small rectangular patches
+    img1Rect = img1[r1[1]:r1[1] + r1[3], r1[0]:r1[0] + r1[2]]
+    img2Rect = img2[r2[1]:r2[1] + r2[3], r2[0]:r2[0] + r2[2]]
+
+    size = (r[2], r[3])
+    warpImage1 = applyAffineTransform(img1Rect, t1Rect, tRect, size)
+    warpImage2 = applyAffineTransform(img2Rect, t2Rect, tRect, size)
+    # Alpha blend rectangular patches
+    imgRect = (1.0 - alpha) * warpImage1 + alpha * warpImage2
+
+    # Copy triangular region of the rectangular patch to the output image
+    img[r[1]:r[1]+r[3], r[0]:r[0]+r[2]] = img[r[1]:r[1]+r[3], r[0]:r[0]+r[2]] * ( 1 - mask ) + imgRect * mask
+    
+    
+#swap算法
+
+# Check if a point is inside a rectangle
+def rectContains(rect, point) :
+    if point[0] < rect[0] :
+        return False
+    elif point[1] < rect[1] :
+        return False
+    elif point[0] > rect[0] + rect[2] :
+        return False
+    elif point[1] > rect[1] + rect[3] :
+        return False
+    return True
+
+
+#calculate delanauy triangle
+def calculateDelaunayTriangles(rect, points):
+    #create subdiv
+    subdiv = cv2.Subdiv2D(rect);
+    
+    # Insert points into subdiv
+    for p in points:
+        subdiv.insert(p) 
+    
+    triangleList = subdiv.getTriangleList();
+    
+    delaunayTri = []
+    
+    pt = []    
+        
+    for t in triangleList:        
+        pt.append((t[0], t[1]))
+        pt.append((t[2], t[3]))
+        pt.append((t[4], t[5]))
+        
+        pt1 = (t[0], t[1])
+        pt2 = (t[2], t[3])
+        pt3 = (t[4], t[5])        
+        
+        if rectContains(rect, pt1) and rectContains(rect, pt2) and rectContains(rect, pt3):
+            ind = []
+            #Get face-points (from 68 face detector) by coordinates
+            for j in range(0, 3):
+                for k in range(0, len(points)):                    
+                    if(abs(pt[j][0] - points[k][0]) < 1.0 and abs(pt[j][1] - points[k][1]) < 1.0):
+                        ind.append(k)    
+            # Three points form a triangle. Triangle array corresponds to the file tri.txt in FaceMorph 
+            if len(ind) == 3:                                                
+                delaunayTri.append((ind[0], ind[1], ind[2]))
+        
+        pt = []        
+            
+    
+    return delaunayTri
+        
+
+# Warps and alpha blends triangular regions from img1 and img2 to img
+def warpTriangle(img1, img2, t1, t2) :
+
+    # Find bounding rectangle for each triangle
+    r1 = cv2.boundingRect(np.float32([t1]))
+    r2 = cv2.boundingRect(np.float32([t2]))
+
+    # Offset points by left top corner of the respective rectangles
+    t1Rect = [] 
+    t2Rect = []
+    t2RectInt = []
+
+    for i in range(0, 3):
+        t1Rect.append(((t1[i][0] - r1[0]),(t1[i][1] - r1[1])))
+        t2Rect.append(((t2[i][0] - r2[0]),(t2[i][1] - r2[1])))
+        t2RectInt.append(((t2[i][0] - r2[0]),(t2[i][1] - r2[1])))
+
+
+    # Get mask by filling triangle
+    mask = np.zeros((r2[3], r2[2], 3), dtype = np.float32)
+    cv2.fillConvexPoly(mask, np.int32(t2RectInt), (1.0, 1.0, 1.0), 16, 0);
+
+    # Apply warpImage to small rectangular patches
+    img1Rect = img1[r1[1]:r1[1] + r1[3], r1[0]:r1[0] + r1[2]]
+    #img2Rect = np.zeros((r2[3], r2[2]), dtype = img1Rect.dtype)
+    
+    size = (r2[2], r2[3])
+
+    img2Rect = applyAffineTransform(img1Rect, t1Rect, t2Rect, size)
+    
+    img2Rect = img2Rect * mask
+
+    # Copy triangular region of the rectangular patch to the output image
+    img2[r2[1]:r2[1]+r2[3], r2[0]:r2[0]+r2[2]] = img2[r2[1]:r2[1]+r2[3], r2[0]:r2[0]+r2[2]] * ( (1.0, 1.0, 1.0) - mask )
+    
+    img2[r2[1]:r2[1]+r2[3], r2[0]:r2[0]+r2[2]] = img2[r2[1]:r2[1]+r2[3], r2[0]:r2[0]+r2[2]] + img2Rect 
+
+if __name__=='__main__':
+    filename1 = '/home/fengchao/桌面/7.jpeg'#面孔模板
+    filename2 = '/home/fengchao/桌面/12.jpeg'#要融合的面孔
+    alpha = 0.5 #alpha调节融合程度，数值越小第一张图融合成分越多
+
+    # 读取图片
+    img1 = cv2.imread(filename1);
+    img2 = cv2.imread(filename2);
+    points1=np.array(get_landmarks(img1)).tolist();
+    points2=np.array(get_landmarks(img2)).tolist();
+    
+    # 将图片像素值转换成浮点数
+    img1 = np.float64(img1)
+    img2 = np.float64(img2)
+    
+    #创建一个list存放morph后的特征点
+    morph_points = []
+    
+    #计算alpha权重特征点获取融合特征点
+    for i in range(0, len(points1)):
+        x = ( 1 - alpha ) * points1[i][0] + alpha * points2[i][0]
+        y = ( 1 - alpha ) * points1[i][1] + alpha * points2[i][1]
+        morph_points.append((x,y))  
+    #np.savetxt("/home/fengchao/桌面/merge points/morph_points.txt",morph_points,fmt='%i %i')
+    
+    #为模板面孔赋值用于生成基于图片一的图片空间
+    morph_img= np.zeros(img1.shape,dtype = img1.dtype)
+    # 从tri.txt文档中读入面部三角
+    with open("tri.txt") as file :
+        for line in file :
+            x,y,z = line.split()
+            
+            x = int(x)
+            y = int(y)
+            z = int(z)
+            
+            t1 = [points1[x], points1[y], points1[z]]
+            t2 = [points2[x], points2[y], points2[z]]
+            t = [morph_points[x], morph_points[y], morph_points[z]]
+            #三角融合算法，对上一步转换后的待融合图片再次取关键点，然后与模特图的关键点一起做三角融合成新的图片
+            morphTriangle(img1, img2, morph_img, t1, t2, t, alpha)
+    
+    # Display Result
+    #cv2.imwrite('/home/fengchao/桌面/merge results/morph_img.jpg', morph_img)
+    cv2.imshow("Morphed Face",np.uint8(morph_img))
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    
+    
+    # Make sure OpenCV is version 3.0 or above
+    (major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
+
+    if int(major_ver) < 3 :
+        print >>sys.stderr, 'ERROR: Script needs OpenCV 3.0 or higher'
+        sys.exit(1)
+
+    # Read images
+    img1=morph_img   #要进行替代的图片
+    img2=cv2.imread(filename1); #图片模板
+    img1Warped = np.copy(img2);    
+    
+    # Read array of corresponding points
+    points1 = np.array(morph_points).tolist();
+    #points2list类型内部存放的是元组
+    points2 = np.array(get_landmarks(img2)).tolist();
+
+    tempt=[]
+    for i in range(0, len(points2)):
+        tempt.append(tuple(points2[i]))
+    points2=tempt
+    # Find convex hull
+    hull1 = []
+    hull2 = []
+
+    hullIndex = cv2.convexHull(np.array(points2), returnPoints = False)
+          
+    for i in range(0, len(hullIndex)):
+        hull1.append(points1[int(hullIndex[i])])
+        hull2.append(points2[int(hullIndex[i])])
+    
+    
+    # Find delanauy traingulation for convex hull points
+    sizeImg2 = img2.shape    
+    rect = (0, 0, sizeImg2[1], sizeImg2[0])
+     
+    dt = calculateDelaunayTriangles(rect, hull2)
+    
+    if len(dt) == 0:
+        quit()
+    
+    # Apply affine transformation to Delaunay triangles
+    for i in range(0, len(dt)):
+        t1 = []
+        t2 = []
+        
+        #get points for img1, img2 corresponding to the triangles
+        for j in range(0, 3):
+            t1.append(hull1[dt[i][j]])
+            t2.append(hull2[dt[i][j]])
+        
+        warpTriangle(img1, img1Warped, t1, t2)
+    
+            
+    # Calculate Mask
+    hull8U = []
+    for i in range(0, len(hull2)):
+        hull8U.append((hull2[i][0], hull2[i][1]))
+    
+    mask = np.zeros(img2.shape, dtype = img2.dtype)  
+    
+    cv2.fillConvexPoly(mask, np.int32(hull8U), (255, 255, 255))
+    
+    r = cv2.boundingRect(np.float32([hull2]))    
+    
+    center = ((r[0]+int(r[2]/2), r[1]+int(r[3]/2)))
+        
+    
+    # Clone seamlessly.
+    output = cv2.seamlessClone(np.uint8(img1Warped), img2, mask, center, cv2.NORMAL_CLONE)
+    
+    cv2.imshow("Face Swapped", output)
+    cv2.waitKey(0)
+    
+    cv2.destroyAllWindows()
+        
+
+
+
+
+
+
